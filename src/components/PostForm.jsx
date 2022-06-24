8cc9b571ac202d7e3f83f9624a1c846fc7c288cf
Neeraja Marri <mnrishi2002@gmail.com>
2022-06-24 14:48:01 +0000

    neeraja adding

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -4,19 +4,21 @@ import { Form, Button } from 'react-bootstrap';
 const PostForm = (props) => {
   const [postTitle, setPostTitle] = useState('');
   const [text, setTextEdit] = useState('');
+  const [authorId, setAuthorId] = useState('');
+  const [pic, setPic] = useState('');
 
 
   const addPost = (e) => {
     e.preventDefault();
     let post = {
       "title": postTitle,
-      "text_post": text
+      "text_post": text,
+      "author": authorId,
+      "picture": pic
     }
     console.log(post);
     props.submitHandler(post);
   }
-
-
 
   return (
     <Form onSubmit={addPost}>
@@ -32,10 +34,35 @@ const PostForm = (props) => {
           onChange={event => setTextEdit(event.target.value)} />
       </Form.Group>
 
+      <Form.Group className="mb-3" controlId="formAuthorId">
+        <Form.Label>AuthorId</Form.Label>
+        <Form.Control type="text" placeholder="Enter Id" value={authorId}
+          onChange={event => setAuthorId(event.target.value)} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formPic">
+        <Form.Label>Pic</Form.Label>
+        <Form.Control type="text" placeholder="Enter picture" value={pic}
+          onChange={event => setPic(event.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="select">
+      <Form.Label>Dropdown Pic Select</Form.Label>
+          <Form.Control as="select" value={pic}
+          onChange={event => setPic(event.target.value)} >
+            <option>France</option>
+            <option>Egypt</option>
+            <option>Ireland</option>
+            <option>Giza</option>
+          </Form.Control>
+        </Form.Group>
+
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
+
   )
 }
 
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,24 +1,50 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
-import one from '../assets/1.jpg';
+// import one from '../assets/xyz.jpg';
 //${props.post.post_id}
 
 
 
 const Posts = (props) => {
   return (
-    <Card style={{ width: '75rem' }}>
-    <Card.Img variant="top" src= {one} />
-    {/* <Card.Img variant="top" src={require(`../assets/${props.post.post_id}.jpg`)} /> */}
-      <Card.Body>  
+    // <Card style={{ width: '75rem' }}>
+    // {/* <Card.Img variant="top" src= {one} /> */}
+    // {/* <Card.Img variant="top" src={require(`../assets/${props.post.picture}.jpg`)} /> */}
+    
+    // if (props.post.picture) {
+    //   <Card.Img variant="top" src={require(`../assets/${props.post.picture}.jpg`)} />
+    // }
+
+    //   <Card.Body>  
+    //     <Card.Title>{props.post.title}</Card.Title>
+    //       <Card.Text>
+    //       {props.post.text_post}
+    //       </Card.Text>
+    //       <Button variant="primary" as={NavLink} to={'/post/'+props.post.text_post}>View Post</Button>
+    //    </Card.Body>
+    // </Card>
+
+    <Row xs={1} md={2} className="g-4">
+{Array.from({ length: 1 }).map((_, idx) => (
+  <Col>
+    <Card>
+    if (props.post.picture) {
+       <Card.Img variant="top" src={require(`../assets/${props.post.picture}.jpg`)} />
+     }
+      <Card.Body>
         <Card.Title>{props.post.title}</Card.Title>
-          <Card.Text>
-          {props.post.text_post}.
-          </Card.Text>
-          <Button variant="primary" as={NavLink} to={'/post/'+props.post.text_post}>View Post</Button>
-       </Card.Body>
+        <Card.Text>
+        {props.post.text_post}
+        </Card.Text>
+        <Button variant="primary">View Post</Button>
+      </Card.Body>
     </Card>
+  </Col>
+))}
+</Row>
+
   )
 }
 
